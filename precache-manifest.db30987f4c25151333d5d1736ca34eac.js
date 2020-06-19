074fa77ecda863095d073a7a711670e0552da891
Ahmed Ollaek <ahmedollaek96@gmail.com>
2020-06-20 00:14:55 +0200

    Updates

---
+++ precache-manifest.db30987f4c25151333d5d1736ca34eac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ecb8c25c1bc448bea4f912833cf52d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "436bc091211b4d0eb6d9",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "76e93cfb2c982b54c494",
+    "url": "./static/js/2.cf80375b.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "./static/js/2.cf80375b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "436bc091211b4d0eb6d9",
+    "url": "./static/js/main.e0efd9bb.chunk.js"
+  },
+  {
+    "revision": "d873b5d3e85ea4d2ebc9",
+    "url": "./static/js/runtime-main.56a780c4.js"
+  }
+]);
